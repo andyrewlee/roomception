83f013f1efb4cd82f1f6c717f82275b1405fefed
Andrew Lee <andyrewlee@gmail.com>
2014-10-02 13:27:54 -0700

    Add functionality and style to earth chat

--- config/routes.js
+++ config/routes.js
@@ -1,12 +1,52 @@
 var users = require('./../server/controllers/users.js');
 
-module.exports = function Routes(app, io){
-  app.get('/', function(req,res) { req.session.page = 'index'; console.log('request', req.session); users.index(req,res) });
-  app.get('/users', function(req,res) { req.session.name = 'mike' ; console.log('REQUEST', req.session); users.index(req,res) });
-  app.get('/users/index.json', function(req,res) { users.index_json(req,res) });
-  app.get('/users/new', function(req,res) { users.new(req,res) });
-  app.post('/users/create', function(req,res) { users.create(req,res) });
-  app.get('/users/:id', function(req,res) { users.show(req,res) });
-  app.get('/users/:id/edit', function(req,res) { users.edit(req,res) });
-  app.post('/users/newUser_json', function(req,res) { users.newUser_json(req,res) });
-};
+module.exports = function Routes(app){
+
+  var roomValues = [{}, {}, {},
+                    {}, {}, {},
+                    {}, {}, {}];
+
+  var roomKeys = ['limbo', 'lust', 'gluttony',
+                 'greed', 'anger', 'heresy',
+                 'violence', 'fraud', 'treachery'];
+
+
+  app.get('/', function(req,res) {
+    users.index(req,res)
+  });
+
+  app.io.route('client:newUserInRoom', function(req) {
+    var roomKey;
+
+    if(req.session.name == undefined) {
+      req.session.name = 'anonymous';
+    }
+
+    req.session.room = req.data.name;
+    req.session.save(function(){
+      for(var i=0; i < roomValues.length; i++) {
+        if(roomKeys[i] == req.session.room) {
+          roomValues[i][req.io.socket.id] = req.session.name;
+          roomKey = i;
+        }
+      }
+
+      req.io.join(req.session.room);
+
+      app.io.room(req.session.room).broadcast('server:newUserInRoom', { connectedUser: req.session.name,
+                                                                        existingUsers: roomValues[roomKey] });
+      req.io.emit('server:existingUsers', roomValues[roomKey] );
+    });
+  });
+
+  app.io.route('disconnect', function(req) {
+    for(var i in roomKeys) {
+      if(roomKeys[i] == req.session.room) {
+        delete roomValues[i][req.io.socket.id];
+        req.io.room(req.session.room).broadcast('server:userLeavingRoom', { disconnectedUser: req.session.name,
+                                                                            existingUsers: roomValues[i] });
+      }
+    }
+  });
+
+}
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -5,30 +5,4 @@ module.exports = {
     function(req, res){
       res.render('./../public/views/index', {title:'Welcome Page'});
     },
-  index_json:
-    function(req, res){
-      User.find({}, function(err, results){
-        res.send(JSON.stringify(results));
-      });
-    },
-  create:
-    function(req, res){
-      var a = new User(req.body);
-      a.save(function(err){
-        if(err){
-          res.send(JSON.stringify(err));
-        }
-        else {
-          res.send('success');
-        }
-      });
-    },
-  show:
-    function(req, res){
-      res.render('./../server/views/users/show', {title:'Welcome Page'});
-    },
-  edit:
-    function(req, res){
-      res.render('./../server/views/users/edit', {title:'Welcome Page'});
-    }
 }
